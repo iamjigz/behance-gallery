c236e4890c398ace6048a942303540fa55f9dd41
Jigno Alfred V. Venezuela <jigzimon@gmail.com>
2018-08-31 10:39:28 +0800

    Updated home and app.global

--- src/app/core/app.globals.ts
+++ src/app/core/app.globals.ts
@@ -5,12 +5,12 @@ import { Injectable } from '@angular/core';
 })
 export class AppGlobals {
   readonly address: string = 'San Pablo, Laguna, PH';
-  readonly mobile: string = '0909-897-3945';
-  readonly email: string = 'gdelosreyes.bos@gmail.com';
+  readonly mobile: string = '0926-011-3502';
+  readonly email: string = 'jigzimon@gmail.com';
 
-  readonly facebook: string = 'https://www.facebook.com/gardelore';
-  readonly instagram: string = 'https://www.instagram.com/gardelore.44';
-  readonly behance: string = 'https://www.behance.net/gdelosreye2dd8';
+  readonly facebook: string = 'https://www.facebook.com/jigzimon';
+  readonly instagram: string = 'https://www.instagram.com/jigzimon';
+  readonly linkedin: string = 'https://www.linkedin.com/in/jigzimon/';
 
   readonly cors: string = 'https://cors-anywhere.herokuapp.com/';
   readonly base: string = `${this.cors}https://api.behance.net/v2`;
